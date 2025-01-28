392f1949eb21b3fef52bcb4330c185a7f06084a0
Responsorble Research <responsorbleresearch@gmail.com>
2025-01-28 22:03:52 +0200

    mmhmm

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import SampleComponent from './components/SampleComponent';
-import './notebook-renderer-react-sample/src/styles.css';
 
 const App: React.FC = () => {
     return (
         <div>
             <h1>My React App</h1>
-            <SampleComponent />
+            
             {/* Additional components or logic can be added here */}
         </div>
     );
